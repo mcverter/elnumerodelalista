656d180ae41064167909d4cc328411ccb68c3479
mcverter <mitchell.verter@gmail.com>
2019-06-19 23:05:42 -0700

    translations

--- translations.js
+++ translations.js
@@ -1,26 +1,181 @@
-function QUESTION_TO_INTERNATIONAL(questionName, language) {
-/*  "qWhatList",
-    "qHowList",
-    "qWhatList",
-    "qHowList",
-    "qWaitTime",
-    "qQuantity",
-    "qCrossing",
-    "qWhatAsylum",
-    "qCFI",
-    "qMano",
-    "qPostCFI",
-    "qWhatHielera",
-    "qWhatDetencion",
-    "qDetencionPrep",
-    "qMyContact",
-    "qLeviticus",
-    "qACAB",
-    "qBusquedas",
-    "qMutualAid"
-*/
-}
+var questions_to_international = {
+  "qWhatList": {"en":[
+      "What is the list?",
+      "The list is an immoral and racist system that the United States uses to limit the number of people who can enter the country."
+    ],
+    "ht":[
+      "Ki sa ki lis la?",
+      "Lis la se yon sistèm imoral ak rasis ke Etazini sèvi ak limite kantite moun ki ka antre nan peyi a."
+    ],
+    "fr":[
+      "Quelle est la liste?",
+      "La liste est un système immoral et raciste que les États-Unis utilisent pour limiter le nombre de personnes pouvant entrer dans le pays"
+    ]},
+  "qHowList": {"en":[
+      `How does the list work?`,
+      `You go to chaparral from 7 in the morning until 9 in the morning to get a number <br/>
+You wait for your number<br/>
+When your number is called, you can go to the other side`
+    ],
+    "ht":[`Kijan lis la travay?`,
+      `Ou ale nan chaparral depi 7 a nan maten jiska 9 nan maten pou jwenn yon nimewo <br/>
+      Ou rete tann pou nimewo ou <br/>
+      Lè yo rele nimewo ou, ou ka ale nan lòt bò a` ],
+    "fr":[
+      `Comment fonctionne la liste?`,
+      `Vous allez à chaparral de 7 heures du matin à 9 heures du matin pour obtenir un numéro <br/>
+Vous attendez votre numéro <br/>
+Lorsque votre numéro est appelé, vous pouvez aller de l'autre côté`
+    ]},
+  "qWaitTime": {"en":[
+      "How much time do I have to wait?",
+      "A lot. One month?  Two months?  Three?"
+    ],
+    "ht":[
+      `Konbyen tan m ap oblije tann?`,
+      `Anpil. Yon mwa? De mwa? Twa? `
+    ],
+    "fr":[
+      "Combien de temps devrai-je attendre?",
+      `Beaucoup. Un mois? Deux mois? Trois? `
+    ]},
+  "qQuantity": {"en":[
+      `How many numbers are called each day?`,
+      `It's different every day. Some days a lot. Some days a little.  Some days none`
+    ],
+    "ht":[
+      `Konbyen nimewo yo rele chak jou?`,
+      `Li diferan chak jou. Kèk jou anpil. Kèk jou yon ti kras. Kèk jou pèsonn`
+    ],
+    "fr":[
+      `Combien de numéros sont appelés chaque jour?`,
+      `C'est différent tous les jours. Certains jours beaucoup. Quelques jours un peu. Certains jours, personne`
+    ]},
+  "qCrossing": {"en":[
+      `What will happen after I cross to the other side?`,
+      `The American authorities will interrogate you about your asylum case. This is called "The Credible Fear Interview "<br/>
+It is possible that you will be put in a detention facility`
+    ],
+    "ht":[
+      `Kisa ki pral rive apre mwen fin travèse lòt bò lanmè a?`,
+      `Otorite Ameriken yo pral entèwoje ou sou ka azil ou an. Yo rele sa a "Entèvyou pè kredib" <br/>
+Li posib ke ou pral mete nan yon sant detansyon`
+    ],
+    "fr":[
+     `Que se passera-t-il après avoir traversé de l'autre côté?`,
+        `Les autorités américaines vous interrogeront sur votre cas d'asile. C’est ce qu’on appelle "L’interview sur la peur crédible" <br/>
+Il est possible que vous soyez placé dans un centre de détention`
+    ]},
+  "qWhatAsylum": {"en":[],
+    "ht":[],
+    "fr":[]},
+  "qCFI": {"en":[],
+    "ht":[],
+    "fr":[]},
+  "qMano": {"en":[],
+    "ht":[],
+    "fr":[]},
+  "qPostCFI": {"en":[],
+    "ht":[],
+    "fr":[]},
+  "qWhatHielera": {"en":[],
+    "ht":[],
+    "fr":[]},
+  "qWhatDetencion": {"en":[],
+    "ht":[],
+    "fr":[]},
+  "qDetencionPrep": {"en":[],
+    "ht":[],
+    "fr":[]},
+  "qMyContact": {"en":[],
+    "ht":[],
+    "fr":[]},
+  "qLeviticus": {"en":[],
+    "ht":[],
+    "fr":[]},
+  "qACAB": {"en":[],
+    "ht":[],
+    "fr":[]},
+  "qBusquedas": {"en":[],
+    "ht":[],
+    "fr":[]},
+  "qMutualAid": {"en":[],
+    "ht":[],
+    "fr":[]},
+};
 
+var questions_defaults = {
+  "qWhatList":[
+    "¿Que es la lista?",
+    "La lista es un sistema inmoral y racista que los Estados Unidos utilizan para limitar el numero de personas que pueden ingresar al pais."
+  ],
+  "qHowList":[
+    `¿Cómo funciona la lista?`,
+    `Usted va a Chaparral desde las 7 de la mañana hasta las 9 de la mañana para obtener un número <br/>
+Esperas tu numero <br/>
+Cuando se llama su número, puede ir al otro lado`
+  ],
+  "qWaitTime":[
+    `¿Cuánto tiempo tendré que esperar?`,
+    `Mucho ¿Un mes? ¿Dos meses? ¿Tres? `
+  ],
+  "qQuantity":[
+    `¿Cuántos números se llaman cada día?`,
+    `Es diferente cada día. Algunos días mucho. Algunos días un poco. Algunos dias ninguno`
+  ],
+  "qCrossing":[
+    `¿Qué pasará después de que cruce al otro lado?`,
+    `Las autoridades estadounidenses lo interrogarán sobre su caso de asilo. Esto se llama "La entrevista de miedo creíble" <br/>
+Es posible que te pongan en un centro de detención`
+  ],
+  "qWhatAsylum":[
+    `Que es asilo?`,
+    `El Derecho de asilo es un derecho internacional de los derechos humanos, que puede disfrutar cualquier
+    persona fuera de su pais de origen en caso de persecucion (Wikipedia)`
+  ],
+  "qCFI":[
+    `Que es "La Entrevista de Meido Credible?`,
+    `Las autoridades americanos van preguntar a usted: "Entonces senor. Porque queires entrar a los estados
+unidos. Porque no puedas quedarse en tu proprio pais. Cual es tu miedo"<br/>
+En este entrevista, debes explicar que tienes un miedo de regresar a tu pais. Este meido es real. Este miedo
+es credible`
+  ],
+  "qMano":[
+    `Cuales son los partes importantes`,
+    `QUE pasa?<br/>
+QUIEN lo hizo?<br/>
+PORQUE lo hizo?<br/>
+Vas por la POLICIA?<br/>
+Hay OTRA PARTE de tu pais donde puedas vivir?`
+  ],
+  "qPostCFI":[
+    `Que va pasar si todo va bien con mi entrevista?`,
+    `Hay varias posibilidades<br/>
+Todos pasan por la detencion de <em>US Customs and Border Patrol</em>. Asi se llama <strong>la
+    hielera</strong><br/>
+Despues de eso hay dos posibilidades en la mayoria de casos
+    <ul>
+    <li>En la mayoria de casos, las familias con ninos que estan menores de edad se dejan libre</li>
+    <li>En la mayoria de casos, los otros van por una detencion de <em>US Immigration and Customs
+      Enforcement</em></li>
+    </ul>`
+  ],
+  "qWhatHielera":[],
+  "qWhatDetencion":[],
+  "qDetencionPrep":[],
+  "qMyContact":[],
+  "qLeviticus":[],
+  "qACAB":[],
+  "qBusquedas":[],
+  "qMutualAid":[]
+};
+
+function QUESTION_TO_INTERNATIONAL(questionName, language) {
+
+}
+/*
+this should just be a big hash with the result ||'ed in the case of default/Spanish
+ */
 function INTERNATIONAL_DIV_TRANSLATIONS(div, language) {
   switch (div) {
     case "mas_informacion":
@@ -119,11 +274,13 @@ function xDaysAgo(language, x) {
 }
 
 function questionToInternational(questionName, language) {
-  debugger;
   var divElement = document.getElementById(questionName);
-  var qAndA = QUESTION_DIV_TRANSLATIONS(questionName, language);
-  divElement.getElementsByTagName("dd")[0].innerHTML = qAndA[0];
-  divElement.getElementsByTagName("dt")[0].innerHTML = qAndA[1];
+  var qAndA = questions_to_international[questionName][language];
+  if (!qAndA || qAndA.length < 2) {
+    qAndA = questions_defaults[questionName];
+  }
+  divElement.getElementsByTagName("dt")[0].innerHTML = qAndA[0];
+  divElement.getElementsByTagName("dd")[0].innerHTML = qAndA[1];
 
 }
 function divToInternational(divName, language) {
