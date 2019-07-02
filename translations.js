6dd24846674c290f782184d154dbfd82dd2aa7d6
mcverter <mitchell.verter@gmail.com>
2019-07-02 09:44:59 -0700

    using hash for DIV translations

--- translations.js
+++ translations.js
@@ -1,4 +1,34 @@
-var questions_to_international = {
+const DIV_DEFAULTS = {
+  "mas_informacion": "MAS INFORMACION",
+  "sobre": "SOBRE",
+  "el_numero_es": "EL NUMERO ES",
+  "el_numero_de_la_lista": "EL NUMERO DE LA LISTA"
+};
+
+var DIVS_TO_INTERNATIONAL = {
+  "mas_informacion": {
+    "en": "MORE INFORMATION",
+    "fr": "PLUS D'INFORMATION",
+    "ht": "PLIS ENFOMASYON",
+  },
+  "sobre": {
+    "en": "ON",
+    "fr": "SUR",
+    "ht": "SOU"
+  },
+  "el_numero_es": {
+    "en": "THE NUMBER IS",
+    "fr": "LE NOMBRE EST",
+    "ht": "NIMEWO A SE",
+  }, "el_numero_de_la_lista": {
+    "en": "THE LIST NUMBER",
+    "fr": "LE NOMBRE DE LA LISTE",
+    "ht": "NIMEWO LIS LA"
+  }
+};
+
+
+var QUESTIONS_TO_INTERNATIONAL = {
   "qWhatList": {
     "am": [
       `
@@ -1056,7 +1086,7 @@ Utilisez cette page pour lancer une recherche: <BR/>
   },
 };
 
-var questions_defaults = {
+var QUESTION_DEFAULTS = {
   "qWhatList": [
     "¿Que es la lista?",
     "La lista es un sistema inmoral y racista que los Estados Unidos utilizan para limitar el numero de personas que pueden ingresar al pais."
@@ -1210,65 +1240,6 @@ Por favor, use esta página para iniciar una búsqueda: <BR/>
   ]
 };
 
-function QUESTION_TO_INTERNATIONAL(questionName, language) {
-
-}
-
-/*
-this should just be a big hash with the result ||'ed in the case of default/Spanish
- */
-function INTERNATIONAL_DIV_TRANSLATIONS(div, language) {
-  switch (div) {
-    case "mas_informacion":
-      switch (language) {
-        case "en":
-          return "MORE INFORMATION";
-        case "fr":
-          return "PLUS D'INFORMATION";
-        case "ht":
-          return "PLIS ENFOMASYON";
-        default:
-          return "MAS INFORMACION";
-      }
-
-    case "sobre":
-      switch (language) {
-        case "en":
-          return "ON";
-        case "fr":
-          return "SUR";
-        case "ht":
-          return "SOU";
-        default:
-          return "SOBRE";
-      }
-
-    case "el_numero_es":
-      switch (language) {
-        case "en":
-          return "THE NUMBER IS";
-        case "fr":
-          return "LE NOMBRE EST";
-        case "ht":
-          return "NIMEWO A SE";
-        default:
-          return "EL NUMERO ES";
-      }
-
-    case "el_numero_de_la_lista":
-      switch (language) {
-        case "en":
-          return "THE LIST NUMBER";
-        case "fr":
-          return "LE NOMBRE DE LA LISTE";
-        case "ht":
-          return "NIMEWO LIS LA";
-        default:
-          return "EL NUMERO DE LA LISTA";
-      }
-  }
-}
-
 function hoy(language) {
   switch (language) {
     case "en":
@@ -1313,19 +1284,17 @@ function xDaysAgo(language, x) {
       return "ayer";
   }
 }
-
+function divToInternational(divName, language) {
+  document.getElementById(divName).innerHTML =
+    DIVS_TO_INTERNATIONAL[divName][language] ||
+    DIV_DEFAULTS[divName];
+}
 function questionToInternational(questionName, language) {
   var divElement = document.getElementById(questionName);
-  var qAndA = questions_to_international[questionName][language];
+  var qAndA = QUESTIONS_TO_INTERNATIONAL[questionName][language];
   if (!qAndA || qAndA.length < 2) {
-    qAndA = questions_defaults[questionName];
+    qAndA = QUESTION_DEFAULTS[questionName];
   }
   divElement.getElementsByTagName("dt")[0].innerHTML = qAndA[0];
   divElement.getElementsByTagName("dd")[0].innerHTML = qAndA[1];
-
-}
-
-function divToInternational(divName, language) {
-  var divElement = document.getElementById(divName);
-  divElement.innerHTML = INTERNATIONAL_DIV_TRANSLATIONS(divName, language);
 }
