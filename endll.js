ddd4437a843c3fe6d0b0cd131f3f48b8689c70dc
mcverter <mitchell.verter@gmail.com>
2019-07-16 10:02:16 -0700

    putting critical reload section in index.html, from endll.js

--- endll.js
+++ endll.js
@@ -1,8 +1,3 @@
-const list_number = 2702;
-const day_of_number = '14';
-const month_of_number = '07';
-const year_of_number = '2019';
-
 /* language is either specified in URL or browser default language */
 const getParameters = window.location.search.substr(1);
 let language = getParameters.match(/language=(\w+)/) ?
@@ -62,55 +57,5 @@ function translateAllContent() {
   translateAllDivs();
   translateAllQuestions();
 }
-/* We only care about date, not time. */
-const list_date = new Date(year_of_number + '-' + month_of_number + '-' + day_of_number + 'T12:00:00');
-const today = new Date(); //new Date().toJSON().slice(0,10) + 'T12:00:00');
-
-function translateDate() {
-  document.getElementById("list-day").innerText = list_date.toLocaleDateString(language, {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    }
-  )
-}
-
-function numDaysAgo() {
-  const days_difference = dateFns.differenceInCalendarDays(new Date(), list_date);
-  if (days_difference === 0) {
-    return `(${hoy(language)})`;
-  } else if (days_difference === 1) {
-    return `(${ayer(language)})`;
-  } else {
-    return "(hace " + days_difference + "dias)";
-  }
-}
-
-function populateDivs() {
-  const listNumberDiv = document.getElementById("list-number");
-  const daysAgoDiv = document.getElementById("days-ago");
-
-  document.getElementById.innerText = translateDate();
-  listNumberDiv.innerText = list_number.toString();
-  daysAgoDiv.innerText = numDaysAgo();
-}
-
-function reloadIfPreviousDayNumberAndAfter7AMTJ() {
-  const rightNow = new Date();
-  const seven_am_tj = new Date(rightNow.toDateString() + " 07:00:00 GMT-0700 (Pacific Daylight Time)");
-
-  if ((
-      dateFns.differenceInCalendarDays(
-        rightNow, list_date) !== 0) &&
-    (rightNow.getTime() > seven_am_tj.getTime())
-  ) {
-    location.reload(true)
-  }
-}
 
 
-function reloadAfterInterval() {
-  const interval = 3 * 1000;
-  window.setTimeout(reloadIfPreviousDayNumberAndAfter7AMTJ, interval);
-}
