e2a28a46f15be2d075fa5554136bea86f719f1d3
mcverter <mitchell.verter@gmail.com>
2019-12-21 07:37:43 -0600

    rethinking boilerplate

--- Tapachula/app/components/GuttersnipeMap/constants.js
+++
@@ -1,6 +0,0 @@
-const shareables = require("./tj.json").placemarks;
-console.log('shareables', shareables)
-const center = {lat: 32.5364307, lng:-117.037085};
-const zoom = 12;
-
-export {shareables, center, zoom}
--- Tapachula/app/components/GuttersnipeMap/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import GuttersnipeMap from './GuttersnipeMap';
-import {shareables, center, zoom} from './constants';
-
-const SpecificMap = () => (
-  <GuttersnipeMap shareable={shareables} center={center} zoom={zoom}/>
-)
-
-export default SpecificMap;
